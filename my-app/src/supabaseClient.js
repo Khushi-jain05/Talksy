39a24434d0b0595609ee99c8b3152c93607d26e0
Khushi Jain <kj5369227@gmail.com>
2025-07-29 09:38:42 +0530

    .

--- my-app/src/supabaseClient.js
+++ my-app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://qiazitxeswmdypzdmntt.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYXppdHhlc3dtZHlwemRtbnR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzOTUxMDYsImV4cCI6MjA2Nzk3MTEwNn0.zcgOyMTe47_YAWmGWqlgGvb7tJYAb5m1ZZXaF1mKmWY"
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
